53f2c10474884f1bd69b5a84effa5b08a42bef85
Rivaldz <rivalzsetyo@gmail.com>
2019-10-11 20:39:41 +0700

    membuat file java untuk penghitungan

---
+++ app/src/main/java/com/valdo/kalkulatorkalori/util/BasaMetabolicRate.java
@@ -0,0 +1,13 @@
+package com.valdo.kalkulatorkalori.util;
+
+public class BasaMetabolicRate {
+    public static final int MALE = 0;
+    public static final int FEMALE = 1;
+//    public static final int
+
+    private  int tinggi;
+    private  int berat;
+    private  int usia;
+    private  int gender;
+
+}
